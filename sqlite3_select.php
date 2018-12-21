b979e5680c58408e07c41eb9959d8528728bd508
git <1391930327@qq.com>
2018-12-21 11:28:48 +0800

    exec once commit

---
+++ sqlite3_select.php
@@ -0,0 +1,33 @@
+﻿<?php
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('first.db');
+      }
+   }
+   
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Start==/.Opened database successfully\n<br/>";
+	  echo "=====================================\n<br/><br/>";
+   }
+
+   $sql =<<<EOF
+      SELECT * from COMPANY;
+EOF;
+
+   $ret = $db->query($sql);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+      echo "ID=". $row['ID'] . "\n";
+      echo "NAME=". $row['NAME'] ."\n";
+      echo "ADDRESS=". $row['ADDRESS'] ."\n";
+      echo "SALARY=".$row['SALARY'] ."\n\n<br/><br/>";
+   }
+   
+   echo "=====================================\n<br/>";
+   echo "End==/.Operation done successfully\n<br/>";
+   $db->close();
+?>
